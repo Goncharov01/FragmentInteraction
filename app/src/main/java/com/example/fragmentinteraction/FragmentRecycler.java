8dd65c6351cc76b4b4ebb6f2dd0254a48fe93c55
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2020-12-17 23:36:12 +0300

    Preparing to add recycler

---
+++ app/src/main/java/com/example/fragmentinteraction/FragmentRecycler.java
@@ -0,0 +1,28 @@
+package com.example.fragmentinteraction;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentRecycler extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        return inflater.inflate(R.layout.fragment_recycler, container, false);
+    }
+}
